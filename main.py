88704ed0cc9930a9cada28e5707792d8e47f8a27
KsieniiaMaksimo <91872649+kseniamaksimova@users.noreply.github.com>
2022-02-28 08:45:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+x = 0
+for i in range(1,11):
+  x += 1
+  c = x ** 2
+  print(c)
+
+x = 0
+for i in range(1,16):
+  x += 1
+  z = x ** 3
+  print(z)
+
+from math import sqrt
+x = 0
+for i in range(1,21):
+  x += 1
+  v = sqrt(x)
+  print(v)
